32061ce532ff0f97dae10c75c7b8645f47955ee4
dbudell <udell.db@gmail.com>
2015-10-07 19:14:40 -0400

    newest changes

--- hojoko-boston/404.php
+++ hojoko-boston/404.php
@@ -1,4 +1,1 @@
-<?php get_header(); ?>
-    <h1 stlye="text-align:center !important; color:#fff !important;">Page Not Found</h1>
-    <p stlye="text-align:center !important; color:#fff !important;">Sorry, but the page you were trying to view does not exist.</p>
-<?php get_footer(); ?>
+404
